eff85b6f34cd06acf681fb4e586cb899a4b30818
khalid-tourhzaoui <tarikkhalidoss100v3356@gmail.com>
2024-12-22 22:12:30 +0000

    fix erros

--- src/lib/google-ai-model.ts
+++ src/lib/google-ai-model.ts
@@ -1,6 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+if (!apiKey) {
+  throw new Error("API key is not defined");
+}
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
